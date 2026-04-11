e3d912a0db1d4e30af7364d011b05d02338f4ea1
Jonatan Torino <Jonatan.Torino@hotmail.com>
2026-04-11 16:40:09 -0300

    Refactor DeleteSubFolders.ps1 to improve error handling and remove commented-out code

--- DeleteSubFolders.ps1
+++ DeleteSubFolders.ps1
@@ -8,4 +8,15 @@ param (
     [ValidateNotNullOrEmpty]$folderToRemove
 )
 
-Get-ChildItem -Path $path -Recurse -Force -Directory -Include $folderToRemove | Remove-Item -Recurse -Confirm:$false -Force
+# Get-ChildItem -Path $path -Recurse -Force -Directory -Include $folderToRemove | Remove-Item -Recurse -Confirm:$false -Force
+Get-ChildItem -Path $path -Recurse -Force -Directory -Include $folderToRemove -ErrorAction SilentlyContinue |
+ForEach-Object {
+    if (Test-Path -LiteralPath $_.FullName) {
+        try {
+            Remove-Item -LiteralPath $_.FullName -Recurse -Force -ErrorAction Stop
+        }
+        catch {
+            Write-Warning "No se pudo borrar: $($_.FullName) :: $($_.Exception.Message)"
+        }
+    }
+}
